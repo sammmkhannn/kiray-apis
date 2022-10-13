5ba7524cc09b7099f1f76291e4fed33fa35aad50
sam <shoaibur206@gmail.com>
2022-10-14 00:38:45 +0500

    add post controllers

---
+++ controllers/post.controllers.js
@@ -0,0 +1,68 @@
+import Post from "../models/Post.model.js";
+import multer from "multer";
+
+const diskStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + file.originalname);
+  },
+});
+export const upload = multer({ storage: diskStorage });
+
+export const createPost = async (req, res) => {
+  let userId = req.params.userId;
+  let names = [];
+  try {
+    let files = req.files;
+    for (let file of files.slice(0, 5)) {
+      names.push(file.filename);
+    }
+    let newPost = new Post({
+      userId,
+      images: names,
+      location: req.body.location,
+      features: req.body.features,
+      price: req.body.price,
+      bedrooms: req.body.bedrooms,
+      bathrooms: req.body.bathrooms,
+      category: req.body.category,
+      parkings: req.body.parkings,
+      longitude: req.body.longitude,
+      latitude: req.body.latitude,
+    });
+    await newPost.save();
+    return res.status(200).send({
+      success: true,
+      Message: "Your post has been processed to the admin",
+    });
+  } catch (err) {
+    return res.status(200).send({ success: fale, message: err.message });
+  }
+};
+
+export const getAllPosts = async (req, res) => {
+  try {
+    let posts = await Post.find({});
+    posts = posts.map((post) => {
+      post.images = post.images.map((image) => {
+        return process.env.BASE_URI + image;
+      });
+      return post;
+    });
+    return res.status(200).send({ success: true, posts });
+  } catch (err) {
+    return res.status(500).send({ success: fale, Message: err.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  let userId = req.params.userId;
+  try {
+    let posts = await Post.find({ userId });
+    return res.status(200).send({ success: true, posts });
+  } catch (err) {
+    return res.status(500).send({ success: false, Message: err.message });
+  }
+};
