87c0c06260914c11af0836f0673c484553a443ef
sam <shoaibur206@gmail.com>
2022-10-14 10:41:08 +0500

    add user routes

---
+++ routes/user.routes.js
@@ -0,0 +1,8 @@
+import { createUser } from "../controllers/user.controllers";
+import express from "express";
+const router = express.Router();
+import upload from "../models/Post.model";
+
+router.post("/create", upload.single("profile"), createUser);
+
+export default router;
