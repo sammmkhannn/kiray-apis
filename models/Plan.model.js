fa847a26bd5a8e98457e7038a03bb1aa7c537423
sam <shoaibur206@gmail.com>
2022-10-14 10:39:36 +0500

    add plan model

---
+++ models/Plan.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const planSchema = new mongoose.Schema({
+  planType: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  freePosts: {
+    type: Number,
+  },
+  availablePosts: {
+    type: Number,
+  },
+  validity: {
+    type: Number,
+  },
+  issueDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  status: {
+    type: String,
+  },
+});
+
+export default planSchema("Plan", planSchema);
