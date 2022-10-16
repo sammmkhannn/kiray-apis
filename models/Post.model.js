817657bbf760bcd98cdc830ae468990692aae19c
sam <shoaibur206@gmail.com>
2022-10-16 15:46:13 +0000

    add userId prop to the post model

--- models/Post.model.js
+++ models/Post.model.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref:'Post'
+  },
   name: {
     type: String,
   },
