f2e28bf505ec97eb0cb1f267de6d9b1424a1d1e5
sam <shoaibur206@gmail.com>
2022-10-16 14:47:12 +0000

    modify user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
-import { createUser } from "../controllers/user.controllers.js";
+import { register,login,logout } from "../controllers/user.controllers.js";
 import express from "express";
 const router = express.Router();
 import { upload } from "../controllers/post.controllers.js";
 
-router.post("/register", upload.single("profile"), createUser);
-
+router.post("/register", upload.single("profile"), register);
+router.post('/login', login);
+router.put('/logout/:userId',logout);
 export default router;
