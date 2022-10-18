6d4482eea061dec278f6d034c3bedaf924857e18
sam <shoaibur206@gmail.com>
2022-10-18 20:19:15 +0000

    modify plan routes

--- routes/plan.routes.js
+++ routes/plan.routes.js
@@ -1,9 +1,9 @@
-import { subscribe, subscriptionHistory } from "../controllers/plan.controllers.js";
+import { subscribe, subscriptionHistory } from "../controllers/subscription.controllers.js";
 import express from "express";
 const router = express.Router();
 
 
-router.post('/subscribe/:userId', subscribe);
+router.post('/subscribe/:userId/:planId', subscribe);
 router.get('/history/:userId', subscriptionHistory);
 
 export default router;
