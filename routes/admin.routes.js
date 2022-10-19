1ae34f5ba037442c5a9c171180534382a6ab280e
sam <shoaibur206@gmail.com>
2022-10-19 10:32:24 +0500

    modify admin routes

---
+++ routes/admin.routes.js
@@ -0,0 +1,31 @@
+import {
+  register,
+  login,
+  getRegisteredUsers,
+  getTotalUsersCount,
+  postsApproval,
+  approve,
+  cancelApprovalRequest,
+  customerSupportRequests,
+  logout,
+  createPlan,
+  modifyPlan,
+  getAllSubscriptions,
+} from "../controllers/admin.controllers.js";
+import express from "express";
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/registered-users", getRegisteredUsers);
+router.get("/total-users", getTotalUsersCount);
+router.get("/posts-for-approval", postsApproval);
+router.put("/approve/:postId", approve);
+router.put("/cancel-approval-request/:postId", cancelApprovalRequest);
+router.get("/customer-support", customerSupportRequests);
+router.delete("/logout/:userId", logout);
+router.post("/create-plan", createPlan);
+router.put("/update-plan/:planId", modifyPlan);
+router.get("/subscriptions", getAllSubscriptions);
+
+export default router;
