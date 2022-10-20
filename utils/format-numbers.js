225295026027b144549feb25091d5372e47b1adf
sam <shoaibur206@gmail.com>
2022-10-20 11:04:51 +0500

    export default the utils

--- utils/format-numbers.js
+++ utils/format-numbers.js
@@ -1,7 +1,9 @@
-export function formatUSD(stripeAmount) {
+function formatUSD(stripeAmount) {
   return `$${(stripeAmount / 100).toFixed(2)}`;
 }
 
-export function formatStripeAmount(USDString) {
+function formatStripeAmount(USDString) {
   return parseFloat(USDString) * 100;
 }
+
+export default { formatUSD, formatStripeAmount };
