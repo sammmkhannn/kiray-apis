40de587759413ba21996973fd45aa0b94f8bf2d0
sam <shoaibur206@gmail.com>
2022-10-23 11:07:42 +0500

    fix a bug

--- controllers/request.controllers.js
+++ controllers/request.controllers.js
@@ -4,11 +4,11 @@ export const createRequest = async (req, res) => {
   let userId = req.params.userId;
   try {
     let request = new Request({
+      userId: userId,
       fullName: req.body.fullName,
       email: req.body.email,
       subject: req.body.subject,
       description: req.body.description,
-      userId,
     });
     await request.save();
   } catch (err) {
