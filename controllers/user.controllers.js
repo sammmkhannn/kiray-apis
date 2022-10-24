96c7c1dc42036d9144fa7712787191ba65436430
sam <shoaibur206@gmail.com>
2022-10-24 06:55:23 +0500

    modif user controllers

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -20,30 +20,64 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   try {
-    let user = await  User.findOne({ cell: req.body.cell });
-   
+    let user = await User.findOne({ cell: req.body.cell });
+
     if (!user) {
-      return res.status(404).send({ success: true, Message: 'User Not Found!' });
+      return res
+        .status(404)
+        .send({ success: true, Message: "User Not Found!" });
     } else {
-      let accessToken = jwt.sign({ userId: user._id, cell: user.cell }, process.env.ACCESS_TOKEN_SECRET);
+      let accessToken = jwt.sign(
+        { userId: user._id, cell: user.cell },
+        process.env.ACCESS_TOKEN_SECRET
+      );
       let newToken = new Token({
         token: accessToken,
         userId: user._id,
       });
       await newToken.save();
-      return res.status(200).send({ success: true, Message:"Signed In", token: accessToken, user:{_id:user._id,fullName:user.fullName,profile:`${process.env.BASE_URL}/images/${user.profile}`,cell:user.cell}});
+      return res.status(200).send({
+        success: true,
+        Message: "Signed In",
+        token: accessToken,
+        user: {
+          _id: user._id,
+          fullName: user.fullName,
+          profile: `${process.env.BASE_URL}/images/${user.profile}`,
+          cell: user.cell,
+        },
+      });
     }
   } catch (err) {
-    return res.status(500).send({ success:false,Message:err.message})
+    return res.status(500).send({ success: false, Message: err.message });
   }
-}
+};
 
-export const logout = async(req, res) => {
-  let userId = req.param.userId;
+export const logout = async (req, res) => {
+  let userId = req.params.userId;
   try {
     await Token.deleteOne({ userId });
     return res.status(200).send({ success: true, Message: "Logged out!" });
   } catch (err) {
     return res.status(500).send({ success: true, Message: err.message });
   }
-}
+};
+
+export const blockUnblockUser = async (req, res) => {
+  let userId = req.params.userId;
+  try {
+    let user = await User.findOne({ _id: userId });
+    user.blocked = !user.blocked;
+    await user.save();
+    return res
+      .status(200)
+      .send({
+        success: true,
+        Message: user.blocked
+          ? "You blocked this user!"
+          : "You unblocked this user!",
+      });
+  } catch (err) {
+    return res.status(200).send({ success: false, Message: err.message });
+  }
+};
