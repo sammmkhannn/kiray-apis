d7206e4d0e1c93801b19f6cc1b7f85566c4e7a2e
sam <shoaibur206@gmail.com>
2022-10-25 23:39:13 +0500

    modify user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,15 @@
-import { register,login,logout } from "../controllers/user.controllers.js";
+import {
+  register,
+  login,
+  logout,
+  blockUnblockUser,
+} from "../controllers/user.controllers.js";
 import express from "express";
 const router = express.Router();
 import { upload } from "../controllers/post.controllers.js";
 
 router.post("/register", upload.single("profile"), register);
-router.post('/login', login);
-router.put('/logout/:userId',logout);
+router.post("/login", login);
+router.put("/logout/:userId", logout);
+router.put("/block-unblock/:userId", blockUnblockUser);
 export default router;
