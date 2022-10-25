ff0325334ec62679aaa7b70cdfccfdb5d8e4e817
sam <shoaibur206@gmail.com>
2022-10-25 23:38:57 +0500

    modify post routes

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -2,6 +2,8 @@ import {
   createPost,
   getAllPosts,
   getUserPosts,
+  editPost,
+  deletePost,
   upload,
 } from "../controllers/post.controllers.js";
 import express from "express";
@@ -10,5 +12,6 @@ const router = express.Router();
 router.post("/create/:userId", upload.any(), createPost);
 router.get("/all", getAllPosts);
 router.get("/all-user-posts/:userId", getUserPosts);
-
+router.put("/post/edit/:postId", editPost);
+router.delete("/post/delete/:postId", deletePost);
 export default router;
