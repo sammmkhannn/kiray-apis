43b2e39b9cd25349154a11a4de445f432a23e098
sam <shoaibur206@gmail.com>
2022-11-17 12:40:16 +0000

    remove utils

--- utils/format-numbers.js
+++
@@ -1,9 +0,0 @@
-function formatUSD(stripeAmount) {
-  return `$${(stripeAmount / 100).toFixed(2)}`;
-}
-
-function formatStripeAmount(USDString) {
-  return parseFloat(USDString) * 100;
-}
-
-export default { formatUSD, formatStripeAmount };
