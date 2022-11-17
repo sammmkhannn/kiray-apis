156b8e6e9fee58ea25473ffc9f66cec03f421348
sam <shoaibur206@gmail.com>
2022-11-17 12:37:46 +0000

    modify subscription controllers

--- controllers/subscription.controllers.js
+++ controllers/subscription.controllers.js
@@ -1,5 +1,35 @@
 import Subscription from "../models/Subscription.model.js";
 
+export const createSubscription = async (req, res) => {
+  let userId = req.params.userId;
+  try {
+    let subscription = new Subscription({
+      userId,
+      planId: req.body.planId,
+      proudctName: req.body.proudctName,
+      issueDate: req.body.issueDate,
+      expiryDate: req.body.expiryDate,
+      totalPosts: req.body.totalPosts,
+      availablePosts: req.body.availablePosts,
+      active:false,
+    });
+    let sub = await subscription.save();
+    return res.status(200).send({ success: false, Message: "Subscribed the plan",subscription:sub });
+  } catch (err) {
+    return res.status(500).send({ success: false, Message: err.message });
+  }
+}
+
+export const updateSubscription = async (req, res) => {
+  let subscriptionId = req.params.subscriptionId;
+  try {
+    await Subscription.updateOne({ _id: subscriptionId }, req.body);
+    return res.status(200).send({ success: true, Message: "Subscription has been updated!" });
+  } catch (err) {
+    return res.status(500).send({ success: false, Message: err.message });
+  }
+}
+
 export const userSubscriptions = async (req, res) => {
   let userId = req.params.userId;
   try {
@@ -9,7 +39,6 @@ export const userSubscriptions = async (req, res) => {
         .status(404)
         .send({ success: true, Message: "No subscritions found!" });
     }
-    
     return res
       .status(200)
       .send({ success: true, history:subscriptions });
@@ -26,3 +55,6 @@ export const subscriptionHisotry = async (req, res) => {
     return res.status(500).send({ success: false, Message: err.message });
   }
 }
+
+
+
