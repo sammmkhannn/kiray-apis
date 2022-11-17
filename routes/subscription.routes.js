d1a5cf66d96c5d8369f60b52380088edd6f50b8e
sam <shoaibur206@gmail.com>
2022-11-17 12:39:57 +0000

    modify subscription routes

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,11 +1,15 @@
 import {
-  subscribe,
   subscriptionHistory,
+  createSubscription,
+  updateSubscription,
+  userSubscriptions
 } from "../controllers/subscription.controllers.js";
 import express from "express";
 const router = express.Router();
 
-router.post("/subscribe", subscribe);
+router.post("/create", createSubscription);
+router.post("/update", updateSubscription);
+router.post("/subscriptions", userSubscriptions);
 router.get("/history", subscriptionHistory);
 
 export default router;
