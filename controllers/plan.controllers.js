150a87b4d2f0dbea18fef9f71566dc02465f2434
sam <shoaibur206@gmail.com>
2022-11-17 12:34:13 +0000

    add plan controllers

---
+++ controllers/plan.controllers.js
@@ -0,0 +1,60 @@
+import Plan from "../models/Plan.model.js";
+import Admin from "../models/Admin.model.js";
+
+//create product
+export const createPlan = async (req, res) => {
+    try {
+
+        if (!req.file) {
+            return res.status(400).send({ success: false, Message: "Image is required" });
+        }
+        let plan = new Plan({
+            image: req.file.filename,
+            name: req.body.name,
+            amount: req.body.amount,
+            interval: req.body.interval,
+            freePosts: req.body.freePosts,
+        });
+        plan.save().then((response) => {
+            return res.status(200).send({ success: true, Message: "plan has been created!" });
+        }).catch((err) => {
+            return res.status(500).send({ success: false, Message: err.message });
+        });
+
+    } catch (err) {
+        return res.status(500).send({ success: false, Message: err.message });
+
+    }
+};
+
+//get all plans
+export const getAllPlans = async (req, res) => {
+    try {
+        let plans = await Plan.find({});
+        plans = plans.map((plan) => {
+            plan.image = `${process.env.BASE_URL}/images/${plan.image}`;
+            return plan;
+        });
+        let admin = await Admin.findOne();
+        
+        if (plans.length > 0) {
+            return res.status(200).send({ success: true, plans,bankDetails:{bankName:admin.bankName,accountNumber:admin.accountNumber,phoneNumber:admin.phoneNumber} });
+        }
+        return res.status(404).send({ success: false, Message: "Plans Not Found!" });
+    } catch (err) {
+        return res.status(500).send({ success: false, Message: err.message });
+    }
+};
+
+//update plan
+export const updatePlan = async (req, res) => {
+    let planId = req.params.planId;
+    try {
+        await Plan.updateOne({ _id: planId }, req.body);
+        return res.status(200).send({ success: false, Message: 'plan has been updated!' });
+    } catch (err) {
+        return res.status(500).send({ success: false, Message: err.message });
+    }
+};
+
+
