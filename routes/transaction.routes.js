f3df9171a0bf26e1f453e90af2f5dafdb699dab2
sam <shoaibur206@gmail.com>
2022-11-17 19:44:07 +0000

    modify transaction routes

--- routes/transaction.routes.js
+++ routes/transaction.routes.js
@@ -1,12 +1,12 @@
-import { createTransaction, getAllTransactions, approveTransaction, cancelTransaction } from "../controllers/transaction.controllers";
+import { createTransaction, getAllTransactions, approveTransaction, cancelTransaction,adminIncome } from "../controllers/transaction.controllers.js";
 import express from "express";
-
+import { upload } from "../controllers/post.controllers.js";
 const router = express.Router();
 
 
-router.post("/create", createTransaction);
+router.post("/create/:userId", upload.single('image'), createTransaction);
 router.get("/all", getAllTransactions);
 router.put("/approve/:transactionId", approveTransaction);
 router.put("/cancel/:transactionId", cancelTransaction);
-
+router.get("/admin-income",adminIncome)
 export default router;
