38e7bd5e4403c7a5b12831b6856dd2e69326c428
sam <shoaibur206@gmail.com>
2022-11-19 01:13:44 +0500

    fix a bug

--- controllers/post.controllers.js
+++ controllers/post.controllers.js
@@ -18,13 +18,14 @@ export const createPost = async (req, res) => {
   let userId = req.params.userId;
   let names = [];
   try {
-    let subscription = await Subscription.findOne({ Active: true, availablePosts: { $neq: 0 } });
-    if (subscription) {
+   // let subscription = await Subscription.findOne({ Active: true, availablePosts: { $neq: 0 } });
+   /* if (subscription) {
       subscription.availablePosts -= 1;
       if (subscription.availablePosts == 0) {
         subscription.active = false;
       }
       await subscription.save();
+     */
     let files = req.files.length > 5 ? req.files.slice(0, 5) : req.files;
     for (let file of files) {
       names.push(file.filename);
@@ -54,9 +55,9 @@ export const createPost = async (req, res) => {
       success: true,
       Message: "Your post has been processed to the admin",
     });
-    } else {
-      return res.status(400).send({ success: false, Message: "Subscript a plan!" });
-    }
+   // } else {
+     // return res.status(400).send({ success: false, Message: "Subscript a plan!" });
+    //}
   } catch (err) {
     return res.status(500).send({ success: false, message: err.message });
   }
